c6f1e51d565ff460ba2ade24e855adf01b91475f
Patrick Smith <patrick@burntcaramel.com>
2018-01-19 13:08:38 +1100

    Add pluralize

--- src/Inflexio/Pluralize.elm
+++ src/Inflexio/Pluralize.elm
@@ -1,65 +1,83 @@
 module Inflexio.Pluralize
     exposing
         ( singularize
-        -- , pluralize
+        , pluralize
         )
-
 
 import Regex exposing (Regex, regex)
 import Set exposing (Set)
 
 
 uncountable : Set String
-uncountable = Set.fromList
-    [ "aircraft",
-    "bellows",
-    "bison",
-    "deer",
-    "equipment",
-    "fish",
-    "hovercraft",
-    "information",
-    "jeans",
-    "means",
-    "measles",
-    "money",
-    "moose",
-    "news",
-    "pants",
-    "police",
-    "rice",
-    "series",
-    "sheep",
-    "spacecraft",
-    "species",
-    "swine",
-    "tights",
-    "tongs",
-    "trousers"
-    ]
+uncountable =
+    Set.fromList
+        [ "aircraft"
+        , "bellows"
+        , "bison"
+        , "deer"
+        , "equipment"
+        , "fish"
+        , "hovercraft"
+        , "information"
+        , "jeans"
+        , "means"
+        , "measles"
+        , "money"
+        , "moose"
+        , "news"
+        , "pants"
+        , "police"
+        , "rice"
+        , "series"
+        , "sheep"
+        , "spacecraft"
+        , "species"
+        , "swine"
+        , "tights"
+        , "tongs"
+        , "trousers"
+        ]
 
 
 firstCaptureAppending : String -> Regex.Match -> String
 firstCaptureAppending suffix match =
     case match.submatches of
-        Just captured::tl ->
+        (Just captured) :: tl ->
             captured ++ suffix
-        
+
         _ ->
             suffix
 
 
-patternAndSuffix : String -> String -> (Regex.Regex, Regex.Match -> String)
+patternAndSuffix : String -> String -> ( Regex.Regex, Regex.Match -> String )
 patternAndSuffix pattern suffix =
-    (pattern |> regex |> Regex.caseInsensitive, firstCaptureAppending suffix)
+    ( pattern |> regex |> Regex.caseInsensitive, firstCaptureAppending suffix )
 
 
-pattern : String -> (Regex.Regex, Regex.Match -> String)
+patternAndSuffixOneOrTwo : String -> String -> ( Regex.Regex, Regex.Match -> String )
+patternAndSuffixOneOrTwo pattern suffix =
+    let
+        extractFromMatch : Regex.Match -> String
+        extractFromMatch match =
+            case match.submatches of
+                Nothing :: (Just captured) :: tl ->
+                   captured ++ suffix
+                
+                (Just captured) :: Nothing :: tl ->
+                   captured ++ suffix
+
+                _ ->
+                    suffix
+    in
+        ( pattern |> regex |> Regex.caseInsensitive, extractFromMatch )
+
+
+pattern : String -> ( Regex.Regex, Regex.Match -> String )
 pattern pattern =
     patternAndSuffix pattern ""
 
 
-singular : List (Regex, Regex.Match -> String)
+singular : List ( Regex, Regex.Match -> String )
 singular =
     [ patternAndSuffix "(alumn|cact|fung|radi|stimul|syllab)i" "us"
     , patternAndSuffix "(alg|antenn|amoeb|larv|vertebr)ae" "a"
@@ -102,7 +120,52 @@ singular =
     ]
 
 
-find_match : List (Regex, Regex.Match -> String) -> String -> String
+plural : List ( Regex, Regex.Match -> String )
+plural =
+    [ patternAndSuffix "(alumn|cact|fung|radi|stimul|syllab)us" "i"
+    , patternAndSuffix "(alg|antenn|amoeb|larv|vertebr)a" "ae"
+    , patternAndSuffix "^(gen)us$" "era"
+    , patternAndSuffix "(pe)rson" "ople"
+    , pattern "^(zombie)s$"
+    , patternAndSuffix "(g)oose" "eese"
+    , patternAndSuffix "(criteri)on" "a"
+    , pattern "^(men)$"
+    , pattern "^(women)"
+    , patternAndSuffix "^(echo)" "es"
+    , patternAndSuffix "^(hero)" "es"
+    , patternAndSuffix "^(potato)" "es"
+    , patternAndSuffix "^(tomato)" "es"
+    , patternAndSuffix "^(t)ooth" "eeth"
+    , patternAndSuffix "^(l)ouse" "ice"
+    , patternAndSuffix "^(addend|bacteri|curricul|dat|memorand|quant)um$" "a"
+    , patternAndSuffix "^(di)e" "ce"
+    , patternAndSuffix "^(f)oot" "eet"
+    , patternAndSuffix "^(phenomen)on" "a"
+    , patternAndSuffix "(child)$" "ren"
+    , patternAndSuffix "(m)an$" "en"
+    , patternAndSuffix "(m|l)ouse" "ice"
+    , patternAndSuffix "(database)s$" ""
+    , patternAndSuffix "(quiz)$" "zes"
+    , patternAndSuffix "^(ox)$" "en"
+    , patternAndSuffix "(matr|vert|ind)ix|ex$" "ices"
+    , patternAndSuffix "(x|ch|ss|sh)$" "es"
+    , patternAndSuffix "([^aeiouy]|qu)y$" "ies"
+    , patternAndSuffix "(hive)$" "s"
+    , patternAndSuffix "(sc[au]rf)$" "s"
+    , patternAndSuffixOneOrTwo "(?:([^f])fe|((hoo)|([lra]))f)$" "ves"
+    , patternAndSuffix "sis$" "ses"
+    , patternAndSuffix "([ti])um$" "a"
+    , patternAndSuffix "(buffal|tomat)o$" "oes"
+    , patternAndSuffix "(octop|vir)us$" "i"
+    , patternAndSuffix "(bus|alias|status|canvas)$" "es"
+    , patternAndSuffix "(ax|test)is$" "es"
+    , patternAndSuffix "s$" "s"
+    , patternAndSuffix "data$" "data"
+    , patternAndSuffix "()$" "s"
+    ]
+
+
+find_match : List ( Regex, Regex.Match -> String ) -> String -> String
 find_match regexes word =
     if Set.member word uncountable then
         word
@@ -110,23 +173,23 @@ find_match regexes word =
         replace_match regexes word
 
 
-replace_match : List (Regex, Regex.Match -> String) -> String -> String
+replace_match : List ( Regex, Regex.Match -> String ) -> String -> String
 replace_match regexes word =
     let
         test regex word =
             Regex.contains regex word
 
         findRegex word =
-            List.filter (\(regex, _)  -> test regex word) regexes
+            List.filter (\( regex, _ ) -> test regex word) regexes
                 |> List.head
 
         regexAndReplacer =
             findRegex word
     in
         case regexAndReplacer of
-            Just (regex, replacer) ->
+            Just ( regex, replacer ) ->
                 Regex.replace Regex.All regex replacer word
-            
+
             Nothing ->
                 word
 
@@ -134,3 +197,8 @@ replace_match regexes word =
 singularize : String -> String
 singularize word =
     find_match singular word
+
+
+pluralize : String -> String
+pluralize word =
+    find_match plural word
--- tests/PluralizeTest.elm
+++ tests/PluralizeTest.elm
@@ -2,42 +2,68 @@ module PluralizeTest exposing (..)
 
 import Expect exposing (Expectation)
 import Test exposing (..)
-import Inflexio.Pluralize exposing (singularize)
+import Inflexio.Pluralize exposing (singularize, pluralize)
 
 
 suite : Test
 suite =
-    describe "Pluralize"
+    describe "Inflexio.Pluralize."
         [ describe "singularize"
             [ test "common plurals" <|
                 \() ->
                     [ "users", "dogs", "cats", "rabbits", "birds", "owls" ]
                         |> List.map singularize
-                    |> Expect.equal
-                        [ "user", "dog", "cat", "rabbit", "bird", "owl" ]
+                        |> Expect.equal
+                            [ "user", "dog", "cat", "rabbit", "bird", "owl" ]
             , test "interesting plurals" <|
                 \() ->
                     [ "children", "women", "seamen", "mice", "lice", "buses", "canvases", "statuses", "aliases", "databases", "synopses", "theses", "analyses" ]
                         |> List.map singularize
-                    |> Expect.equal 
-                        [ "child", "woman", "seaman", "mouse", "louse", "bus", "canvas", "status", "alias", "database", "synopsis", "thesis", "analysis" ]
+                        |> Expect.equal
+                            [ "child", "woman", "seaman", "mouse", "louse", "bus", "canvas", "status", "alias", "database", "synopsis", "thesis", "analysis" ]
             , test "special case nouns ending in f or fe" <|
                 \() ->
                     [ "lives", "knives", "hooves", "roofs", "loofs", "wolves", "calves", "scarfs", "scurfs", "wharves", "loaves", "leaves" ]
                         |> List.map singularize
-                    |> Expect.equal 
-                        [ "life", "knife", "hoof", "roof", "loof", "wolf", "calf", "scarf", "scurf", "wharf", "loaf", "leaf" ]
+                        |> Expect.equal
+                            [ "life", "knife", "hoof", "roof", "loof", "wolf", "calf", "scarf", "scurf", "wharf", "loaf", "leaf" ]
             , test "special case nouns ending in o" <|
                 \() ->
                     [ "echoes", "heroes", "potatoes", "tomatoes" ]
                         |> List.map singularize
-                    |> Expect.equal 
-                        [ "echo", "hero", "potato", "tomato" ]
+                        |> Expect.equal
+                            [ "echo", "hero", "potato", "tomato" ]
             , test "special case misc" <|
                 \() ->
                     [ "teeth", "sheep", "mice", "children", "women", "seamen", "mice", "lice", "buses", "canvases", "statuses", "aliases", "databases", "synopses", "theses", "analyses", "addenda", "algae", "alumni", "amoebae", "antennae", "bacteria", "cacti", "curricula", "data", "fungi", "genera", "larvae", "memoranda", "menus", "stimuli", "syllabi", "vertebrae", "buses", "databases", "shoes", "zombies", "viri", "geese", "criteria", "radii", "classes", "women", "seamen", "men", "abdomens", "abdomen", "specimens", "specimen", "manager", "managers", "People" ]
                         |> List.map singularize
-                    |> Expect.equal 
-                        [ "tooth", "sheep", "mouse", "child", "woman", "seaman", "mouse", "louse", "bus", "canvas", "status", "alias", "database", "synopsis", "thesis", "analysis", "addendum", "alga", "alumnus", "amoeba", "antenna", "bacterium", "cactus", "curriculum", "datum", "fungus", "genus", "larva", "memorandum", "menu", "stimulus", "syllabus", "vertebra", "bus", "database", "shoe", "zombie", "virus", "goose", "criterion", "radius", "class", "woman", "seaman", "man", "abdomen", "abdomen", "specimen", "specimen", "manager", "manager", "Person" ]
+                        |> Expect.equal
+                            [ "tooth", "sheep", "mouse", "child", "woman", "seaman", "mouse", "louse", "bus", "canvas", "status", "alias", "database", "synopsis", "thesis", "analysis", "addendum", "alga", "alumnus", "amoeba", "antenna", "bacterium", "cactus", "curriculum", "datum", "fungus", "genus", "larva", "memorandum", "menu", "stimulus", "syllabus", "vertebra", "bus", "database", "shoe", "zombie", "virus", "goose", "criterion", "radius", "class", "woman", "seaman", "man", "abdomen", "abdomen", "specimen", "specimen", "manager", "manager", "Person" ]
+            ]
+        , describe "pluralize"
+            [ test "common plurals" <|
+                \() ->
+                    [ "user", "dog", "cat", "rabbit", "bird", "owl" ]
+                        |> List.map pluralize
+                        |> Expect.equal
+                            [ "users", "dogs", "cats", "rabbits", "birds", "owls" ]
+            , test "special case nouns ending in f or fe" <|
+                \() ->
+                    [ "life", "knife", "hoof", "roof", "loof", "wolf", "calf", "scarf", "scurf", "wharf", "loaf", "leaf" ]
+                        |> List.map pluralize
+                        |> Expect.equal
+                            [ "lives", "knives", "hooves", "roofs", "loofs", "wolves", "calves", "scarfs", "scurfs", "wharves", "loaves", "leaves" ]
+            , test "special case nouns ending in o" <|
+                \() ->
+                    [ "echo", "hero", "potato", "tomato" ]
+                        |> List.map pluralize
+                        |> Expect.equal
+                            [ "echoes", "heroes", "potatoes", "tomatoes" ]
+            , test "special case misc" <|
+                \() ->
+                    [ "tooth", "sheep", "mouse", "child", "woman", "seaman", "mouse", "louse", "bus", "canvas", "status", "alias", "database", "synopsis", "thesis", "analysis", "addendum", "alga", "alumnus", "amoeba", "antenna", "bacterium", "cactus", "curriculum", "datum", "fungus", "genus", "larva", "memorandum", "menu", "stimulus", "syllabus", "vertebra", "bus", "database", "shoe", "zombie", "virus", "goose", "criterion", "radius", "class", "woman", "seaman", "man", "abdomen", "abdomens", "specimen", "specimens", "manager", "managers", "Person" ]
+                        |> List.map pluralize
+                        |> Expect.equal
+                            [ "teeth", "sheep", "mice", "children", "women", "seamen", "mice", "lice", "buses", "canvases", "statuses", "aliases", "databases", "synopses", "theses", "analyses", "addenda", "algae", "alumni", "amoebae", "antennae", "bacteria", "cacti", "curricula", "data", "fungi", "genera", "larvae", "memoranda", "menus", "stimuli", "syllabi", "vertebrae", "buses", "databases", "shoes", "zombies", "viri", "geese", "criteria", "radii", "classes", "women", "seamen", "men", "abdomens", "abdomens", "specimens", "specimens", "managers", "managers", "People" ]
             ]
         ]
